c49540cd254a187833f1ac821bd486c9237b39cf
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-09-02 14:45:41 +0900

    BaseGameScene.js を更新

--- src/scenes/BaseGameScene.js
+++ src/scenes/BaseGameScene.js
@@ -98,7 +98,7 @@ export default class BaseGameScene extends Phaser.Scene {
      * エディタからオブジェクト追加の依頼を受けた時の、デフォルトの処理。
      * 子クラスで、このメソッドをオーバーライドすることを想定。
      */
-    addObjectFromEditor(assetKey) {
+    addObjectFromEditor(assetKey, newName) {
         console.warn(`[BaseGameScene] addObjectFromEditor is not implemented in '${this.scene.key}'.`);
         return null;
     }
