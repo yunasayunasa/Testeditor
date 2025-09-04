9ecb2f99d64cb3a3d70bc18cb4718cf9830b3bbf
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-09-04 19:28:13 +0900

    vis

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,13 +2,13 @@
 
 import CoinHud from './CoinHud.js';
 import HpBar from './HpBar.js';
-import VirtualStick from './VirtualStick.js';
+//import VirtualStick from './VirtualStick.js';
 // import JumpButton from './JumpButton.js'; // 将来の追加
 
 // このカタログに登録するだけで、UISceneが新しいUIを自動で認識できるようになる
 export const CUSTOM_UI_MAP = {
     'CoinHud': CoinHud,
     'HpBar': HpBar,
-    'VirtualStick': VirtualStick,
+   // 'VirtualStick': VirtualStick,
     // 'JumpButton': JumpButton,
 };
