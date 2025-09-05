3f3e35333501d07d724f3bd5604a2fa62b0c498f
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-09-05 18:58:40 +0900

    prs

--- src/handlers/scenario/r.js
+++ src/handlers/scenario/r.js
@@ -1,13 +1,16 @@
-// src/handlers/r.js
 /**
- * [r] タグ - 選択肢表示 (Render choices)
- * [link]で溜めた選択肢を画面に表示する。
+ * [r] タグ - 改行 兼 選択肢表示 (Render)
+ * 
+ * メッセージウィンドウをクリアし、[link]で定義された選択肢を画面に表示します。
+ * このタグ自体はクリックを待ちません。待機は[s]タグが担当します。
+ * 
+ * @param {ScenarioManager} manager - ScenarioManagerのインスタンス
+ * @param {object} params - パラメータ (このタグでは使用しません)
  */
-export function handleReturn(manager, params) {
-    if (manager.scene.pendingChoices && manager.scene.pendingChoices.length > 0) {
+export default async function handleR(manager, params) {
+    
+    // GameSceneに選択肢ボタンの表示を命令する
+    if (manager.scene.pendingChoices.length > 0) {
         manager.scene.displayChoiceButtons();
     }
-    // isWaitingClick や isWaitingChoice のフラグは立てない！
-    // 待機は[s]タグに任せる
-    return Promise.resolve();
 }
