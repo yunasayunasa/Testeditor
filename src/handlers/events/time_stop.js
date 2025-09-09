ba000737fa9dee159cf30e1b51e6e3e207dfdb7d
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-09-10 01:53:22 +0900

    タイム

--- src/handlers/events/time_stop.js
+++ src/handlers/events/time_stop.js
@@ -1,11 +1,17 @@
-// Odyssey Engine - [time_stop] Action Tag
-// Stops the master clock of the engine.
-//
+// time_stop.js
 
 export default async function timeStop(interpreter, target, params) {
     const systemScene = interpreter.scene.game.scene.getScene('SystemScene');
     if (systemScene) {
-        systemScene.isTimeStopped = true;
-      console.log(`%c[LOG BOMB 4] time_stop handler: Setting isTimeStopped to TRUE.`, 'color: red; font-size: 1.2em; font-weight: bold;');
-      }
+        
+        // ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+        // ★★★ これが、全てを解決する、最後の魔法だ ★★★
+        // ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+        // 処理を、次のフレームに遅延させる
+        interpreter.scene.time.delayedCall(0, () => {
+            console.log(`%c[LOG BOMB 4] time_stop handler: Setting isTimeStopped to TRUE. (Delayed)`, 'color: red; font-size: 1.2em; font-weight: bold;');
+            systemScene.isTimeStopped = true;
+        });
+        
+    }
 }
