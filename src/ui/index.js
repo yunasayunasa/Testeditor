e1f36655e2b0fb6ee13564f04467dbe65a1d7af2
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-09-15 01:02:28 +0900

    オーバーレイ

--- src/ui/index.js
+++ src/ui/index.js
@@ -38,7 +38,7 @@ export const uiRegistry = {
 export const sceneUiVisibility = {
     'GameScene': ['hud', 'menu', 'game'], // 'controls' はないのでスティックは出ない
       'NovelOverlayScene': ['game'], // メッセージウィンドウ('game'グループ)だけを表示
-    'JumpScene': ['controls', 'action','game'], // 'hud' 'menu' はないのでHUDやメニューは出ない
+    'JumpScene': ['controls', 'action',], // 'hud' 'menu' はないのでHUDやメニューは出ない
     'BattleScene': ['hud', 'battle'],
      'ActionScene': ['menu', 'game'],
     'TitleScene': []
