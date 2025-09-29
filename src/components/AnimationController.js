ba46c301e916022358f168fe7d273d000e095642
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-09-29 21:17:16 +0900

    アニメ

--- src/components/AnimationController.js
+++ src/components/AnimationController.js
@@ -3,126 +3,83 @@
 export default class AnimationController {
     constructor(scene, owner, params = {}) {
         this.gameObject = owner;
+        // ★★★ "scene"引数は使わず、ownerから直接シーンを取得する ★★★
+        this.scene = owner.scene; 
 
         this.animPrefix = params.prefix || owner.getData('anim_prefix') || owner.name.split('_')[0]; 
         
-        console.log(`[AnimationController] Initialized for '${owner.name}'. Using animation prefix: '${this.animPrefix}'`);
+        console.log(`[AnimationController] Initialized for '${owner.name}'. Using prefix: '${this.animPrefix}'. Scene key: '${this.scene.scene.key}'`);
         
-        // --- 1. 内部状態を先に保持 ---
         this.lastState = 'idle';
         this.lastDirection = 'right';
-
-        
-
-       
     }
+    
     start() {
-        // --- イベントリスナーをここで登録 ---
         if (this.gameObject.on) {
             this.gameObject.on('onStateChange', this.handleStateChange, this);
             this.gameObject.on('onDirectionChange', this.handleDirectionChange, this);
         }
-  // ▼▼▼【ここから究極のデバッグコード】▼▼▼
-    console.log(`[AnimationController | DEBUG] Attempting to play initial animation for '${this.gameObject.name}'...`);
-    console.log('[AnimationController | DEBUG] >> Is my gameObject a Sprite?', this.gameObject.constructor.name);
-    console.log('[AnimationController | DEBUG] >> Which scene do I belong to?', this.gameObject.scene.scene.key);
-    console.log('[AnimationController | DEBUG] >> Does my scene have the anims?', this.gameObject.scene.anims.get(`${this.animPrefix}_idle`));
-    console.log('[AnimationController | DEBUG] >> Does my OWN anims property have the anims?', this.gameObject.anims.get(`${this.animPrefix}_idle`));
-
-    // ★★★ この行でブラウザが一時停止します ★★★
-    debugger; 
-
-    this.updateAnimation();
-    
+        this.updateAnimation();
     }
 
-    // stateが変化したときに呼ばれる
     handleStateChange(newState, oldState) {
-        this.lastState = newState;
-        this.updateAnimation();
-    }
-
-    // directionが変化したときに呼ばれる
-    handleDirectionChange(newDirection, oldDirection) {
-        this.lastDirection = newDirection;
-        this.updateAnimation();
-    }
-
-    // in src/components/AnimationController.js
-
-// ... (constructorや他のメソッドはそのまま) ...
-
-// アニメーションを更新するメインロजिक
-updateAnimation() {
-    if (!this.gameObject || !this.gameObject.scene || !this.gameObject.active || !this.gameObject.play) return;
-    
-    let directionSuffix = this.lastDirection;
-    let flipX = false;
-    
-    if (directionSuffix.includes('left')) {
-        flipX = true;
-        directionSuffix = directionSuffix.replace('left', 'right');
-    }
-    
-    let animKey;
-    if (this.lastState === 'idle') {
-        animKey = `${this.animPrefix}_idle`;
-    } else {
-        animKey = `${this.animPrefix}_${this.lastState}_${directionSuffix}`;
-    }
-    
-    // ▼▼▼【ここから修正版デバッグコード】▼▼▼
-    
-    // 安全に現在のアニメーションキーを取得する
-    const currentAnimKey = this.gameObject.anims.currentAnim ? this.gameObject.anims.currentAnim.key : null;
-    
-    // ログを出力して状況を把握する
-    // console.log(`[AnimationController] State: '${this.lastState}', Direction: '${this.lastDirection}'. Attempting to play key: '${animKey}'`);
-    
-    // 再生しようとしているアニメーションが、既に再生中のものと同じなら、何もしない
-    if (currentAnimKey === animKey) {
-        return; 
-    }
-
-    // 目的のアニメーションが存在するかチェック
-    if (this.gameObject.anims.exists(animKey)) {
-        // console.log(`%cSUCCESS: Animation key '${animKey}' found. Playing.`, 'color: green; font-weight: bold;');
-        this.gameObject.setFlipX(flipX);
-        this.gameObject.play(animKey, true);
-    } else {
-        // 存在しない場合、エラーをコンソールに出力
-        console.error(`%c[AnimationController] ERROR: Animation key '${animKey}' NOT FOUND!`, 'color: red; font-weight: bold;');
-        
-        // 代わりにアイドルアニメーションを再生しようと試みる (フォールバック)
-        const idleKey = `${this.animPrefix}_idle`;
-        if (currentAnimKey !== idleKey && this.gameObject.anims.exists(idleKey)) {
-            this.gameObject.play(idleKey, true);
-        } else if (!this.gameObject.anims.exists(idleKey)) {
-            // アイドルすらない場合はアニメーションを停止
-            this.gameObject.stop();
+        if (this.lastState !== newState) {
+            this.lastState = newState;
+            this.updateAnimation();
         }
     }
-    // ▲▲▲【ここまで修正版デバッグコード】▲▲▲
-}
 
-// ... (destroyメソッドなどはそのまま) ...
+    handleDirectionChange(newDirection, oldDirection) {
+        if (this.lastDirection !== newDirection) {
+            this.lastDirection = newDirection;
+            this.updateAnimation();
+        }
+    }
+
+    updateAnimation() {
+        // ★★★ 安全装置を追加 ★★★
+        if (!this.gameObject || !this.gameObject.active || !this.scene) return;
+        
+        let directionSuffix = this.lastDirection;
+        let flipX = false;
+        
+        if (directionSuffix.includes('left')) {
+            flipX = true;
+            directionSuffix = directionSuffix.replace('left', 'right');
+        }
+        
+        let animKey;
+        if (this.lastState === 'idle') {
+            animKey = `${this.animPrefix}_idle`;
+        } else {
+            animKey = `${this.animPrefix}_${this.lastState}_${directionSuffix}`;
+        }
+
+        const currentAnimKey = this.gameObject.anims.currentAnim ? this.gameObject.anims.currentAnim.key : null;
+        if (currentAnimKey === animKey) {
+            return; 
+        }
+
+        // ★★★ シーンのアニメーションマネージャーを直接参照して存在確認 ★★★
+        if (this.scene.anims.exists(animKey)) {
+            this.gameObject.setFlipX(flipX);
+            this.gameObject.play(animKey, true);
+        } else {
+            console.error(`[AnimationController] ERROR: Animation key '${animKey}' NOT FOUND in scene '${this.scene.scene.key}'!`);
+            
+            const idleKey = `${this.animPrefix}_idle`;
+            if (currentAnimKey !== idleKey && this.scene.anims.exists(idleKey)) {
+                this.gameObject.play(idleKey, true);
+            } else {
+                this.gameObject.stop();
+            }
+        }
+    }
 
     destroy() {
-        // コンポーネントが破棄されるときに、リスナーを解除する
-        if (this.gameObject.off) {
+        if (this.gameObject && this.gameObject.off) {
             this.gameObject.off('onStateChange', this.handleStateChange, this);
             this.gameObject.off('onDirectionChange', this.handleDirectionChange, this);
         }
     }
 }
-
-/*
-アニメーションキーの命名規則:
-アニメーションキーは、必ず**[プレフィックス]_[状態]_[向き]**という形式で命名しなければならない。
-例: player_walk_right, slime_idle_down
-（idleのように向きがないものは[プレフィックス]_[状態]）
-アニメーションの対称性:
-左向きのアニメーション（walk_leftなど）は作らず、右向きのアニメーション（walk_right）を**setFlipX(true)で反転させて表現する**ことを基本とする。
-（左右非対称なキャラクターの場合は、AnimationControllerを少し改造して、walk_leftも個別に再生できるようにすることも可能です）
-コンポーネントの依存関係:
-AnimationControllerは、同じオブジェクトにPlayerControllerやNpcControllerのような、onStateChangeとonDirectionChangeイベントを発火させるコンポーネントが存在することを前提とする。*/
