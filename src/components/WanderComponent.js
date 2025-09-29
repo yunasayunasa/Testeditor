ecdf6ff727687fcfe9ac3f3e7c3eee05168fb96d
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-09-29 23:48:44 +0900

    ワンダー

--- src/components/WanderComponent.js
+++ src/components/WanderComponent.js
@@ -1,19 +1,12 @@
-// in src/components/WanderComponent.js (8方向対応・最終FIX版)
 // in src/components/WanderComponent.js
 
 export default class WanderComponent {
     constructor(scene, owner, params = {}) {
         this.gameObject = owner;
         this.npcController = null;
-        
-        // ▼▼▼【ここが修正の核心】▼▼▼
-        // paramsはコンストラクタでプロパティに保存せず、
-        // updateメソッド内で直接参照するように変更する
-        this.params = params; 
-        // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
-
         this.timer = 0;
         this.state = 'WAITING';
+        // constructorでは、paramsをプロパティに保存しない！
     }
 
     start() {
@@ -22,8 +15,11 @@ export default class WanderComponent {
             console.error(`[WanderComponent] ERROR: 'NpcController' component is required!`);
             return;
         }
-        // ★★★ start()が呼ばれた時点の正しい待機時間でタイマーをセット ★★★
-        const waitDuration = this.params.waitDuration ?? 2000;
+
+        // ▼▼▼【ここが修正の核心】▼▼▼
+        // start()が実行された時点の最新のパラメータを取得する
+        const myParams = this.getCurrentParams();
+        const waitDuration = myParams.waitDuration ?? 2000;
         this.timer = this.gameObject.scene.time.now + waitDuration;
     }
 
@@ -31,16 +27,19 @@ export default class WanderComponent {
         if (!this.npcController) return;
 
         if (this.gameObject.scene.time.now > this.timer) {
-            // ★★★ updateが実行される時点の正しいパラメータを取得 ★★★
-            const walkDuration = this.params.walkDuration ?? 3000;
-            const waitDuration = this.params.waitDuration ?? 2000;
-            const is8Way = this.params.is8Way ?? false;
+            // ▼▼▼【ここも同様に修正】▼▼▼
+            // updateが実行される時点の最新のパラメータを取得する
+            const myParams = this.getCurrentParams();
+            const walkDuration = myParams.walkDuration ?? 3000;
+            const waitDuration = myParams.waitDuration ?? 2000;
+            const is8Way = myParams.is8Way ?? false;
             
             if (this.state === 'WAITING') {
                 this.state = 'WALKING';
                 this.timer = this.gameObject.scene.time.now + walkDuration;
                 
                 let vx = 0;
+                // ★ NpcControllerのmoveSpeedも、常に最新のものを参照する
                 const speed = this.npcController.moveSpeed;
 
                 if (is8Way) {
@@ -56,6 +55,16 @@ export default class WanderComponent {
                 this.npcController.stop();
             }
         }
+    }
+
+    /**
+     * ★★★ 新規ヘルパーメソッド ★★★
+     * gameObjectのデータから、自分自身の最新のパラメータ定義を取得する
+     */
+    getCurrentParams() {
+        const allComponentsData = this.gameObject.getData('components') || [];
+        const myComponentData = allComponentsData.find(c => c.type === 'WanderComponent');
+        return myComponentData ? myComponentData.params : {};
     }
 }
 
