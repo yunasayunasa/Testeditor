358bf8003b1fa7f9773e1578f7c0011703891fe9
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-10-07 08:55:26 +0900

    await

--- src/handlers/scenario/jump.js
+++ src/handlers/scenario/jump.js
@@ -36,7 +36,7 @@ export default async function handleJump(manager, params) {
         
 
         // ★★★ SystemSceneを直接呼ぶ代わりにEngineAPIを呼ぶ ★★★
-        EngineAPI.requestSimpleTransition(fromSceneKey, toSceneKey, transitionParams);
+       await EngineAPI.requestSimpleTransition(fromSceneKey, toSceneKey, transitionParams);
 
 manager.stop();
         
