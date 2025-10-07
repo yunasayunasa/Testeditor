1a40a9e2b86dd1231b8185a8415dcb210efd7cf5
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-10-07 16:23:42 +0900

    Update close_menu.js

--- src/handlers/events/close_menu.js
+++ src/handlers/events/close_menu.js
@@ -1,7 +1,11 @@
-// src/handlers/events/close_menu.js
+// src/handlers/events/close_menu.js (修正後)
 import EngineAPI from '../../core/EngineAPI.js';
+
 export default async function close_menu(interpreter) {
-    EngineAPI.requestCloseMenu(interpreter.scene.scene.key);
+    // ★ 汎用的な「閉じる」メソッドを呼び出す
+    EngineAPI.requestCloseOverlay(interpreter.scene.scene.key);
+
+    // ★ 遷移系タグではないので、'__interrupt__' は不要
 }
 
 close_menu.define = {
