8e8d5840b20e8534a6bb9cf31cc3483024d56161
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-10-11 16:30:46 +0900

    playbgn

--- src/handlers/events/play_bgm.js
+++ src/handlers/events/play_bgm.js
@@ -10,3 +10,15 @@ export default async function play_bgm(interpreter, params) {
         });
     }
 }
+
+
+/**
+ * ★ VSLエディタ用の自己定義 ★
+ */
+play_bgm.define = {
+    description: 'BGM（背景音楽）を再生します。すでに別のBGMが再生中の場合は、クロスフェードして切り替わります。',
+    params: [
+        { key: 'key', type: 'asset_key', assetType: 'audio',label: 'BGMアセット名', defaultValue: '' },
+        { key: 'loop', type: 'select', options: ['true', 'false'], label: 'ループ再生', defaultValue: true }
+    ]
+};
