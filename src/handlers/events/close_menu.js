57adad0b56051c2a5f92d9f8d42ba8dea7fd2ed1
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-11-07 16:33:14 +0900

    ｔｔ

--- src/handlers/events/close_menu.js
+++ src/handlers/events/close_menu.js
@@ -1,8 +1,28 @@
-// src/handlers/events/close_menu.js (最終FIX・改訂版)
+// in src/handlers/events/close_menu.js
 import EngineAPI from '../../core/EngineAPI.js';
 
 export default async function close_menu(interpreter) {
-    // ★「ゲームを再開してくれ」と、CEOにお願いする
+    
+    // ▼▼▼【ここからが、最後の修正です】▼▼▼
+    
+    const currentScene = interpreter.scene;
+    
+    if (currentScene && currentScene.scene.key === 'OverlayScene') {
+        console.log(`[close_menu] Destroying all objects in '${currentScene.scene.key}' before closing...`);
+        
+        // 1. 自分が所属するシーンの、全てのゲームオブジェクトを破棄する
+        [...currentScene.children.list].forEach(child => {
+            child.destroy();
+        });
+    }
+    
+    // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+
+    // 2. (これまでの処理) GameFlowManagerにイベントを発行する
     EngineAPI.fireGameFlowEvent('CLOSE_PAUSE_MENU');
+    
+    // 3. (これまでの処理) VSLの実行を中断する
     return '__interrupt__';
 }
+
+// defineプロパティは変更なし
